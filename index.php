a7ef488f65de3c8017e2e42413fa07c367ea0410
Leom-Lin <ls476304858@gmail.com>
2019-11-19 21:37:45 +0800

    2019.11.16广东工业大学移动智能信息系统实验大作业aService

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "<body><div>用户登陆名：".$_POST['user_name']
+    ."<br>真实姓名：".$_POST['real_name']
+    ."<br>真实年龄：".$_POST['real_lage']
+    ."<br>出生日期：".$_POST['birthday']
+    ."<br>所在城市：".$_POST['s_province'].$_POST['s_city']
+    ."<br>电子邮箱：".$_POST['myemail']
+    ."<br>身份证号：".$_POST['card']
+    ."<br>手机号码：".$_POST['telphone']
+    ."<br>个人主页：".$_POST['myurl']
+//     ."<br>图片：".$_POST['mypic']
+    ."<br>个人简介：".$_POST['ir']
+    ."<br>留言：".$_POST['message']
+    ."<br>幸运颜色代码：".$_POST['lovecolor']."<//div><//body>";
+
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />";
